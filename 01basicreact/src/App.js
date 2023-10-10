2b5113c07cd1508c44cb5988614b99e840941af0
rishabh <rishabhrj1508@gmail.com>
2023-10-10 12:54:41 +0530

    learned how to create react project using vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai Aur React | In this , i have learned how to create react project using two methods , this one is create-react-app method</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Chai Aur React With Vite | I learned how to create react project using vite</h1>
+  )
+}
+
+export default App
