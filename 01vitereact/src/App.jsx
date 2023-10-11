9da87f4fab7be06e56096e17bd56d08c66957c73
rishabh <rishabhrj1508@gmail.com>
2023-10-11 11:01:05 +0530

    created custom React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,12 @@
 import Chai from "./chai";
+
 function App() {
+  const username = "chai aur code";
 
   return (
     <>
     <Chai />
-    <h1>Chai aur react</h1>
+      <h1>Chai aur react in { username }</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App!</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google!!'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Click Me to Visit google !!!!!</a>
+)
+
+const anotherUser = "hey there";
+
+const reactElement = React.createElement(
+    'a',
+    { href: 'https://gooogle.com', target: '_blank' },
+    'Click Me !!',
+    anotherUser
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    
+    reactElement
 )
